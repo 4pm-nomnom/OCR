b8f08ab669857c3446db5cd8d14fc37aa12c4013
Pierrick MADE <34524195+Pierrick-MADE@users.noreply.github.com>
2018-10-11 17:32:16 +0200

    update ocr.c (added steps to the main program)

--- ocr.c
+++ ocr.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+// #include <SDL2/SDL.h> // <SDL.h> ?
+// #include <SDL2/SDL_image.h>  // <SDL_image.h> ? // used to load the image
+// #include <gtk/gtk.h> // graphical user interfaces
 
-int main()
+// #include "image_loading.h"
+// #include "preprocessing.h"
+// #include "character_detection.h"
+// #include "character_recognition.h"
+// #include "postprocessing.h"
+// #include "graphical_interface.h"
+
+
+int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
-    return 0;
+    printf("Hello World! The OCR is strating\n");
+    
+    /************************************************************
+    *                      Image Loading                        *
+    * This step convert an image (png/jpg/bmp...) to a matrix.  *
+    *************************************************************/
+    
+    /************************************************************
+    *                   Image Pre-processing                    *
+    * Process the image to make the character detection &       *
+    * recognition easier.                                       *
+    *************************************************************/
+    
+    //--- change the color of the image from color to grey scale-  
+    
+    
+    
+    /************************************************************
+    *                  Character Recognition                    *
+    * Convert the input matrix (representing a character) to an *
+    * ASCII                                                     *
+    *************************************************************/
+    
+    //__init__ // when init the network, random value instead of nothing 
+    
+        
+    
+    return EXIT_SUCCESS;
 }
