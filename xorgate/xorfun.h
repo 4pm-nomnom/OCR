6f83555d09ecdc2740a4968f19b52346d831217f
Victor Dutto <victor.dutto@epita.fr>
2018-10-21 17:18:29 +0200

    NeuralNetwork, feedforward working, but not backprop
    Signed-off-by: Victor Dutto <victor.dutto@epita.fr>

--- xorgate/xorfun.h
+++ xorgate/xorfun.h
@@ -2,5 +2,6 @@
 #define	XORFUN_H
 
 float xorfun(float a, float b);
-
+float speand1(float a, float b);
+float speand2(float a, float b);
 #endif
--- xorgate/xorgate.h
+++ xorgate/xorgate.h
@@ -1,11 +1,49 @@
 #ifndef XORGATE_H
 #define XORGATE_H
+#include "nodexor.h"
 
+
+typedef struct InputLayer
+{
+	NodeXorDouble Node1, Node2;
+	
+} InputLayer;
+
+typedef struct HiddenLayer
+{
+	NodeXor Node1, Node2;
+	
+} HiddenLayer;
+
+typedef struct OutputLayer
+{
+	float res;
+	float bias;
+	
+} OutputLayer;
+
+
+typedef struct NeuralXorNet
+{
+	InputLayer InputLayer;
+	HiddenLayer HiddenLayer;
+	OutputLayer OutputLayer;
+
+} NeuralXorNet;
+
+float feedforward(NeuralXorNet A, float x1, float x2);
+float ffnode1(InputLayer I, HiddenLayer H, float x1, float x2);
+float ffnode2(InputLayer I, HiddenLayer H, float x1, float x2);
+float ff(HiddenLayer H, OutputLayer O);
 float fsquare(float x);
-float xorgate(float x1, float x2, float w1, float w2, float bias);
-float cost(float x1, float x2, float w1, float w2, float bias);
-int rosenberg (float x1, float x2, float w1, float w2, float bias);
-float[] train(float w1, float w2, float b);
+float costout(NeuralXorNet A, float x1, float x2);
+float costhidden1(NeuralXorNet A, float x1, float x2);
+float costhidden2(NeuralXorNet A, float x1, float x2);
+float operroro(HiddenLayer H, OutputLayer O, float x1, float x2);
+float operrorh1(InputLayer I, HiddenLayer H, float x1, float x2);
+float operrorh2(InputLayer I, HiddenLayer H, float x1, float x2);
+NeuralXorNet stochastic(NeuralXorNet A, float x1, float x2);
+NeuralXorNet visualepoch(NeuralXorNet A);
+NeuralXorNet trainepoch(NeuralXorNet A, short i);
 
 #endif
-
