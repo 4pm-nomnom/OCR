41f376c75828d0c55b8d9fb36a39ad0aa3dc15a1
Cloé Lacombe <cloe.lacombe@epita.fr>
2018-11-27 19:30:52 +0100

    Sorry ca marche plus. probleme de loop.

--- Neural_Network_Imtryingplsbenice/neural_network.c
+++ Neural_Network_Imtryingplsbenice/neural_network.c
@@ -4,13 +4,13 @@
 #include <time.h>
 #include "setting_variables.h"
 
-double sum_weights(double* input, double* weight, size_t len) 
+double sum_weights(double* input, double* weight)
 // c'est la fonction de base pour caculer ce que ton neuronne doit calculer en gros,
 // weight[0] c'est le bias, et le reste du tableau weight c'est les weights de chacun 
 // de tes inputs.
 {
 	double sum = weight[0];
-	for (size_t i = 1; i < len; ++i)
+	for (size_t i = 1; weight[i] != 0; ++i)
 		sum += input[i -  1] * weight[i];
 // input[i-1] car :
 // tableau weights : [bias, w1, w2, w3, ..., wn]
@@ -34,10 +34,10 @@ double sigmoid_prime(double val)
 }
 
 
-void feedforward_layer(double* input, double** layer, size_t nbNeurones, size_t nbWeights)
+void feedforward_layer(double* input, double** layer)
 {
 	double* temp = malloc(sizeof(double) * nbNeurones);
-	for (size_t i = 0; i < nbNeurones; ++i)
+	for (size_t i = 0; layer[i] != 0; ++i)
 	// Tu fais ton iteration sur chacun des neuronnes de ta couche
 		temp[i] = sigmoid(sum_weights(input, layer[i], nbWeights));
 		// si t'as pas compris cette partie t'es debile et t'as mon numero
@@ -45,10 +45,10 @@ void feedforward_layer(double* input, double** layer, size_t nbNeurones, size_t 
 	free(temp);
 }
 
-void feedforward(double* input, double*** network, size_t nblayer)
+void feedforward(double* input, double*** network)
 {
-	for (size_t i = 0; i < nblayer; ++i)
-		feedforward_layer(input, network[i], sizeof(network[i]) / sizeof(double*), sizeof(input)/sizeof(double));
+	for (size_t i = 0; network[i] != 0; ++i)
+		feedforward_layer(input, network[i]);
 }
 
 
@@ -74,38 +74,38 @@ double diff_weight(double learning_rate, double urgence, double input)
 	return learning_rate * urgence * input;
 }
 
-void backprop_node(double* node, size_t len, double* input, double expected, double output)
+void backprop_node(double* node, double* input, double expected, double output)
 {
 	node[0] += diff_bias(0.3, urgence(expected, output));
-	printf("%f ", node[0]);
-	for (size_t i = 0; i < len; ++i)
+	printf("bias = %f\n", node[0]);
+	for (size_t i = 1; node[i] != 0; ++i)
 		{
 			node[i] += diff_weight(0.3, urgence(expected, output), input[i-1]);
-			printf("%f ", node[i]);
+			printf("weight i = %f ", node[i]);
 		}
 	printf("\n");
 }
 
 
-void backprop_layer(double** layer,size_t len_node, size_t len, double* input, double* expected, double* output)
+void backprop_layer(double** layer, double* input, double* expected, double* output)
 {
-	for (size_t i = 0; i < len; i++)
-		backprop_node(layer[i], len_node, input, expected[i], output[i]);
+	for (size_t i = 0; layer[i] != 0; i++)
+		backprop_node(layer[i], input, expected[i], output[i]);
 }
 
-void backprop(double*** network, double* expected, double* output, double* input, size_t nblayers)
+void backprop(double*** network, double* expected, double* output, double* input, size_t nblayer)
 {
-	for (size_t i = 0; i < nblayers; ++i)
+	for (size_t i = 0; network[i] < nblayer; ++i)
 		backprop_layer(network[i], sizeof(network[i][0])/sizeof(double), sizeof(network[i]) /sizeof(double*), input, expected, output);
 }
 
 
 void epoch(double*** network, double* input, double* expected)
 {
-	size_t nblayer = sizeof(network)/sizeof(double**);
+	size_t nblayer = 2;
 	double* output = copy_array(input, sizeof(input) / sizeof(double));
-	feedforward(output, network, nblayer);
-	backprop(network, expected, output, input, nblayer);
+	feedforward(output, network, 2);
+	backprop(network, expected, output, input, 2);
 	free(output);
 }
 
@@ -116,12 +116,13 @@ int main()
 	double *input = malloc(sizeof(double) * 2);
 	input[0] = (double) 1;
 	input[1] = (double) 1;
-	double ***network = (double***) (malloc(sizeof(double**) * 2));
-	network[0] = (double**) (malloc((sizeof(double*) * 2)));
-	network[1] = (double**) (malloc((sizeof(double*))));
-	network[0][0] = (double*) (malloc(sizeof(double) * 3));
-	network[0][1] = (double*) (malloc(sizeof(double) * 3));
-	network[1][0] = (double*) (malloc((sizeof(double)) * 3));
+	double ***network;
+	network = malloc(2*sizeof(double**));
+	network[0] = malloc(2*sizeof(double*));
+	network[1] =  malloc(sizeof(double*));
+	network[0][0] = malloc(sizeof(double) * 3);
+	network[0][1] = malloc(sizeof(double) * 3);
+	network[1][0] = malloc(sizeof(double) * 3);
 	fill_array_random(network[0], 2, 3);
 	fill_array_random(network[1], 1, 3);
 	double* expected = malloc(sizeof(double));
