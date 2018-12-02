f7891abd58aa6ba04cb22c24560c028100ef546c
Cloé Lacombe <cloe.lacombe@epita.fr>
2018-12-02 16:28:07 +0100

    NN_last_commit
    Signed-off-by: Cloé Lacombe <cloe.lacombe@epita.fr>

--- NN_as_Arrays/neural_net.c
+++ NN_as_Arrays/neural_net.c
@@ -5,7 +5,7 @@
 #include "setting_variables.h"
 #include "maths.h"
 
-
+// computes each node output with respect to inputs
 void feedforward_layer(double* input, double** layer, size_t nbNeurones, double* inputs)
 {
 	for (size_t i = 0; i < nbNeurones; ++i)
@@ -14,7 +14,7 @@ void feedforward_layer(double* input, double** layer, size_t nbNeurones, double*
 	}
 }
 
-
+//gives the output with respect to a vector inputs
 void feedforward(double*** network, double** inputs, double* output)
 {
 	size_t i;
@@ -25,7 +25,7 @@ void feedforward(double*** network, double** inputs, double* output)
 	feedforward_layer(inputs[i], network[i], nbneurones[i], output);
 }
 
-
+//update a node error margin with respect to the cost of the feedforward output
 double backprop_node_out(double target, double output, double* inputs, size_t nbWeight)
 {
 	double error_out = 0;
@@ -36,14 +36,14 @@ double backprop_node_out(double target, double output, double* inputs, size_t nb
 	return error_out;
 }
 
-
+// calls node error for the whole last layer
 void backprop_layer_out(double* inputs, double* outputs, double* errors_out, double* targets, size_t nbNeurones)
 {
 	for (size_t i = 0; i < nbNeurones; ++i)
 		errors_out[i] = backprop_node_out(targets[i], outputs[i], inputs, nbweights[i]);
 }
 
-
+//update a node error margin
 double backprop_node(double* node, double* target, double* out, double output, double* inputs, size_t nbWeights)
 {
 	double error = 0;
@@ -55,7 +55,8 @@ double backprop_node(double* node, double* target, double* out, double output, d
 	return error; 
 }
 
-
+// Instantiate the errors of the whole layer
+// in is the inputs of the layer in the network associated to the layer of the error matrix
 void backprop_layerH(double** layer, double* targets, double* output, double* out, double* in, double* errors, size_t nbNodes)
 {
 	for (size_t i = 0; i < nbNodes - 1; ++i)
@@ -64,6 +65,8 @@ void backprop_layerH(double** layer, double* targets, double* output, double* ou
 	}
 }
 
+
+// Update each weight and bias according to its error margin
 void correct(double*** network, double** error)
 {
 	for (size_t i = 0; i < 2; ++i)
@@ -76,7 +79,10 @@ void correct(double*** network, double** error)
 		}
 }
 
-
+//outputs is the vector given by the feedforward
+// target is the value expected to reach
+//inputs is  the vector having the inputs
+// computes the error margin for each node, then update the weights according to its error margin
 void backprop(double*** network, double* outputs, double* target, double** inputs)
 {
 	double** errors;
@@ -93,6 +99,10 @@ void backprop(double*** network, double* outputs, double* target, double** input
 	correct(network, errors);
 }
 
+
+//Calls the feedforward then the backpropagation 
+// for given inputs
+//knowing the expected result
 void epoch(double*** network, double* input, double* expected)
 {
 	double **inputs;
@@ -104,9 +114,29 @@ void epoch(double*** network, double* input, double* expected)
 	output = malloc(sizeof(double));
 	feedforward(network, inputs, output);
 	backprop(network, output, expected, inputs);
-//	printf("Output = %f\n", output[0]);
 }
 
+
+//Calls the feedforward then the backpropagation 
+// for given inputs
+//knowing the expected result
+// Printing the output 
+void displaying_epoch(double*** network, double* input, double* expected)
+{
+	double **inputs;
+	inputs = malloc(2 * sizeof(double*));
+	inputs[0] = malloc(sizeof(double) * 2);
+	inputs[0] = input;
+	inputs[1] = malloc(sizeof(double) * 2);
+	double* output;
+	output = malloc(sizeof(double));
+	feedforward(network, inputs, output);
+	backprop(network, output, expected, inputs);
+	printf("Post_correction output = %f\n", output[0]);
+}
+
+
+//calls several time inputs
 void Train(double*** network)
 {
 		for (size_t j = 0; j < 5000; ++j)
@@ -114,13 +144,14 @@ void Train(double*** network)
 				size_t i = rand() % 4;
 				double* input = malloc(sizeof(double) * 2);
 				input = _input[i];
-//				printf("\nInput 1 = %f, Input 2 = %f\n", input[0], input[1]);
 				double* expected = malloc(sizeof(double));
 				expected[0] = (double) (i%2) ;
 				epoch(network, input, expected);
 		}
 }
 
+
+//Pretty print of all nodes weights in the network
 void print_network(double*** network, size_t i)
 {
 	for (size_t _i = 0; _i < i; ++_i)
@@ -151,6 +182,16 @@ void Neural_network()
 		print_network(network, nblayer);
 		Train(network);
 		print_network(network, nblayer);
+		double *input = malloc(sizeof(double) * 2);
+		double* expected = malloc(sizeof(double));
+		for (size_t i = 0; i < 4; ++i)
+		{
+			input = _input[i];
+			printf("Input1 is %f	|| Input2 is %f\n", input[0], input[1]);
+			expected[0] = (double) (i % 2);
+			displaying_epoch(network, input, expected);
+		}
+		
 }
 
 int main()
